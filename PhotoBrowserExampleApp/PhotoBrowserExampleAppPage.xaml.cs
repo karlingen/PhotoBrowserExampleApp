f5a9a98a12149942e3515e5b4858c97afb4aa37a
Karl Metum <karlo.m@gmail.com>
2018-03-04 21:36:56 +0100

    initial

---
+++ PhotoBrowserExampleApp/PhotoBrowserExampleAppPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PhotoBrowserExampleApp
+{
+    public partial class PhotoBrowserExampleAppPage : ContentPage
+    {
+        public static int currentImageId { get; set; } = 50;
+
+        public PhotoBrowserExampleAppPage()
+        {
+            InitializeComponent();
+
+            updateImage();
+        }
+
+        void HandleLeftButtonClicked(object sender, System.EventArgs e)
+        {
+            currentImageId--;
+            updateImage();
+        }
+
+        void HandleRightButtonClicked(object sender, System.EventArgs e)
+        {
+            currentImageId++;
+            updateImage();
+        }
+
+        private void updateImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri($"https://picsum.photos/1920/1080?image={currentImageId}"),
+                CachingEnabled = true
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Photo Browser
+
+This is an example app built with Xamarin.Forms for demonstrating how to browse through a set of images downloaded remotely from the interwebz and showing an ActivityIndicator while doing so.
+
+# Setup
+1. Clone the repo
+2. Open the solution in either VS for Mac or Visual Studio.
+3. Build and Run.
